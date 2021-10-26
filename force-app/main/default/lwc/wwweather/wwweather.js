1c8eadd0b550eac865ff6d60d23295d410cb718c
AndreiKapranov-cloud <76791760+AndreiKapranov-cloud@users.noreply.github.com>
2021-10-26 14:24:42 +0300

    Update wwweather.js

--- force-app/main/default/lwc/wwweather/wwweather.js
+++ force-app/main/default/lwc/wwweather/wwweather.js
@@ -42,108 +42,12 @@ export default class weather extends LightningElement {
     }
   }
 
-  /*   loadBears() {
-    this.isFormEnabled = true;
-		refreshWeather({
-            city : this.displayCity,
-           
-        })
-			.then(result => {
-                
-				this.objList = result;
-			})
-			.catch(error => {
-				this.error = error;
-			});
-	}*/
+  
 
   catch(error) {
     console.error(error);
     this.error = error;
   }
 
-  /*handleFormSave() {
-    this.handleRefresh();
-}*/
 
-  /*  @wire(refreshWeather) forecast__cs;
-    error;
-    configuration;
-    //apiKey;
-    displayCity;
-    temperature;
-   weatherDescription;
-    weatherIcon;
-    timestamp;
-    isFormEnabled;
-    thisDate;
-    thisTime;
-  //  objList;
- /*     connectedCallback() {
-        this.getLastSync();
-    }
-  async getLastSync() {
-        this.error = '';
-        try {
-            this.configuration = await getLastSyncDetails();
-            this.populateValues();
-        }
-        catch (error) {
-            console.error(error);
-            this.error = error;
-        }
-    }
-*/
-
-  /*   async handleRefresh() {
-        this.error = '';
-        try {
-            this.configuration = await refreshWeather({
-                city : this.displayCity
-            });
-            this.populateValues();
-        }
-        catch (error) {
-            console.error(error);
-            this.error = error;
-        }
-    }
-    populateValues() {
-        const configuration = this.configuration;
-        this.isFormEnabled = false;
-      //  this.objList=this.objList;
-       // if(configuration) {
-        for (var i = 0; i < forecast__cs.length; i++) { 
-           this.displayCity = configuration.City__c;
-            this.temperature = configuration.Temperature__c;
-          this.weatherDescription = configuration.Weather_message__c;
-       }
-         
-        }
-      // else {
-      //     this.isFormEnabled = true;
-     //   }
-    
-    updateValue(event) {
-        let element = event.target.name;
-        let value = event.target.value;
-        if(element === 'inputCity') {
-            this.displayCity = value;
-        }
-     /*   if(element === 'inputApiKey') {
-            if(value !== '') {
-                this.apiKey = value;
-            }
-        }*/
-  /*  }
-    handleFormSave() {
-        this.handleRefresh();
-    }
-    handleEdit() {
-        this.isFormEnabled = true;
-    }
-    handleCancel() {
-        this.isFormEnabled = false;
-    }
-}*/
 }
